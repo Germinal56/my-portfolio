b78afc4f269a92b6b32a897e4db5d5c8a8c6c63a
Germinal56 <stockhudson@mail.com>
2025-12-28 21:20:15 +0100

    Updated eslint

--- src/lib/mongoose.ts
+++ src/lib/mongoose.ts
@@ -9,19 +9,31 @@ if (!MONGODB_URI) {
 /**
  * Reuse the cached connection in dev to avoid creating many connections
  */
-let cached: { conn: typeof mongoose | null; promise: Promise<typeof mongoose> | null } = (global as any).mongoose;
+type MongooseCache = {
+  conn: typeof mongoose | null;
+  promise: Promise<typeof mongoose> | null;
+};
+
+declare global {
+  // eslint-disable-next-line no-var
+  var mongoose: MongooseCache | undefined;
+}
+
+let cached = global.mongoose;
 
 if (!cached) {
-  cached = (global as any).mongoose = { conn: null, promise: null };
+  cached = global.mongoose = { conn: null, promise: null };
 }
 
 export async function dbConnect() {
-  if (cached.conn) return cached.conn;
-  if (!cached.promise) {
-    cached.promise = mongoose.connect(MONGODB_URI, {
-      dbName: "GF-DB", // change if you prefer another db name
+  if (cached!.conn) return cached!.conn;
+
+  if (!cached!.promise) {
+    cached!.promise = mongoose.connect(MONGODB_URI, {
+      dbName: "GF-DB",
     });
   }
-  cached.conn = await cached.promise;
-  return cached.conn;
+
+  cached!.conn = await cached!.promise;
+  return cached!.conn;
 }
